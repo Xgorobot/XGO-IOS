f4655934a0c37a3268c60ee47798b0bb0d9c01c5
arther <artheremail@163.com>
2024-10-22 11:47:52 +0800

    提交修改

--- xgo/NewViews/control/NewActionVC.swift
+++ xgo/NewViews/control/NewActionVC.swift
@@ -110,6 +110,7 @@ class NewActionVC: NewsBaseViewController {
                      FindControlUtil.moveX(speed: 0x25)
                     break
                 case .OStop:
+                    FindControlUtil.stopX()
                     FindControlUtil.turnClockwise(speed: 0x80)
                     break
                 default:
